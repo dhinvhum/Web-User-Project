155754ed0a99627262392638fe47a39e69e400e6
dhinvhum <48542992+dhinvhum@users.noreply.github.com>
2019-03-14 19:38:15 +0700

    Add files via upload

---
+++ Project I/index.js
@@ -0,0 +1,38 @@
+
+$("#container2").hide();
+$('#next').click(function() {
+   $('#container1').hide();
+   $('#container2').fadeIn();
+    $(document).ready(async () => {
+        
+        let data = await $.getJSON("./data.json");
+        let selectData = randomSelectData(data)
+        showDisplayData(selectData, $);
+    });
+    
+
+    
+    function randomSelectData(data) {
+        let randomIndex = ( Math.random() * data.length * 10 ) % data.length;
+        randomIndex = Math.floor(randomIndex);
+        console.log("randomIndex", randomIndex)
+        return data[randomIndex];
+    }
+    
+    function showDisplayData(data, $) {
+        $('#pic-card').append(`<img class="picture-img" src=${data.pathPic} alt="">`);
+        $('#des').append(`<p1 id="paragraph1">${data.text}</p1>`);
+    }
+});
+
+$('#back').click(function(){
+    
+    $('#container2').hide();
+    $("#container1").fadeIn();
+
+    $('#pic-card').empty();
+    $('#des').empty();
+    
+})
+
+
